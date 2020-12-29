9338df57bfc242f26bf74649eba0c9d7f2499cd4
Hamid Raza <hamidkhan2582@gmail.com>
2020-12-29 05:15:07 +0500

    first commit

---
+++ src/index.php
@@ -0,0 +1,27 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf610
+{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red232\green135\blue8;\red252\green252\blue252;\red0\green0\blue0;
+\red26\green125\blue169;\red77\green80\blue81;\red41\green142\blue11;\red148\green108\blue71;\red255\green255\blue255;
+}
+{\*\expandedcolortbl;;\cssrgb\c93333\c60000\c0;\cssrgb\c99216\c99216\c99216;\cssrgb\c0\c0\c0;
+\cssrgb\c9804\c56471\c72157;\cssrgb\c37255\c38824\c39216;\cssrgb\c18431\c61176\c3922;\cssrgb\c65098\c49804\c34902;\cssrgb\c100000\c100000\c100000\c49804;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl420\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?php\cf4 \strokec4  \
+\
+\pard\pardeftab720\sl420\partightenfactor0
+\cf5 \strokec5 namespace\cf4 \strokec4  vitalis\cf6 \strokec6 \\\cf4 \strokec4 HelloWorld\cf6 \strokec6 ;\cf4 \strokec4 \
+\
+\cf5 \strokec5 class\cf4 \strokec4  \cf5 \strokec5 Index\cf4 \strokec4 \
+\pard\pardeftab720\sl420\partightenfactor0
+\cf6 \strokec6 \{\cf4 \strokec4 \
+    \cf5 \strokec5 public\cf4 \strokec4  \cf5 \strokec5 function\cf4 \strokec4  \cf7 \strokec7 greet\cf6 \strokec6 (\cf8 \cb9 \strokec8 $greet\cf4 \cb3 \strokec4  \cf8 \cb9 \strokec8 =\cf4 \cb3 \strokec4  \cf7 \strokec7 "Hello World"\cf6 \strokec6 )\cf4 \strokec4 \
+    \cf6 \strokec6 \{\cf4 \strokec4 \
+        \cf5 \strokec5 return\cf4 \strokec4  \cf8 \cb9 \strokec8 $greet\cf6 \cb3 \strokec6 ;\cf4 \strokec4 \
+    \cf6 \strokec6 \}\cf4 \strokec4 \
+\cf6 \strokec6 \}\cf4 \strokec4 \
+}
